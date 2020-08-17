1778927328a444e0170c21b4034c77cacdc93996
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-17 12:09:38 -0600

    had the createTask function take information from the form, and save it to the task ul

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,22 @@ var createTaskHandler = function() {
 
     event.preventDefault();
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "this is a new task";
-    taskItemEl.className = "task-item";
-    tasksToDoEl.appendChild(taskItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 formEl.addEventListener("submit", createTaskHandler);
