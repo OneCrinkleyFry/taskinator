f9a11bbbb3d79de80ffa3e802c2bf517ff86e468
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-18 22:27:19 -0600

    added an edit function that pulls the task info to the Form in the header, and changes the text of the button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,11 +96,33 @@ var deleteTask = function(taskId) {
     taskSelected.remove();
 };
 
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id, taskId");
+    
+};
+
 //handles the button that is being targeted, and calls the functions that handles those.
 var taskButtonHandler = function(event) {
+
+    var targetEl = event.target;
+
     if (event.target.matches(".delete-btn")) {
-        var taskId = event.target.getAttribute("data-task-id");
+        var taskId = targetEl.getAttribute("data-task-id");
         deleteTask(taskId);
+    }
+    else if (event.target.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
     }
 };
 
