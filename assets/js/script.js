64e9662910b489e79ebf8c9594eb7fac9c9fec72
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-19 22:27:25 -0600

    created a drop event handler, and a function to reassign a task to the list it was dropped in.

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,6 +72,7 @@ var createTaskActions = function(taskId){
     return actionContainerEl;
 };
 
+// takes a task, and replaces the current content with those in the button.
 var completeEditTask = function(taskName, taskType, taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
 
@@ -83,6 +84,29 @@ var completeEditTask = function(taskName, taskType, taskId) {
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
 };
+
+//deletes the list item
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+//assigns the task to be edited, moves it to the form, and passes it to the completeEdit function to finish the job.
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
+};
+
+//Event handlers:
 
 // gets the input from the user, and validates it, creates a task object and passes it to the createTaskEl function.
 var taskFormHandler = function() {
@@ -114,28 +138,7 @@ var taskFormHandler = function() {
     }
 };
 
-//deletes the list item
-var deleteTask = function(taskId) {
-    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
-    taskSelected.remove();
-};
-
-var editTask = function(taskId) {
-    console.log("editing task #" + taskId);
-
-    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
-    var taskName = taskSelected.querySelector("h3.task-name").textContent;
-    var taskType = taskSelected.querySelector("span.task-type").textContent;
-
-    document.querySelector("input[name='task-name']").value = taskName;
-    document.querySelector("select[name='task-type']").value = taskType;
-    document.querySelector("#save-task").textContent = "Save Task";
-
-    formEl.setAttribute("data-task-id", taskId);
-
-};
-
-//handles the button that is being targeted, and calls the functions that handles those.
+//determines which button is being pushed, and passes it to the appropriate function.
 var taskButtonHandler = function(event) {
 
     var targetEl = event.target;
@@ -150,6 +153,7 @@ var taskButtonHandler = function(event) {
     }
 };
 
+//finds the task that the event is attached to appends the task to the correct list.
 var taskStatusChangeHandler = function(event) {
     var taskId = event.target.getAttribute("data-task-id");
     var statusValue = event.target.value.toLowerCase();
@@ -164,18 +168,18 @@ var taskStatusChangeHandler = function(event) {
     else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
-
 };
 
+//when the task is picked up, sets an identifier on the task to be found when dropped.
 var dragTaskHandler = function(event) {
     var taskId = event.target.getAttribute("data-task-id");
 
     event.dataTransfer.setData("text/plain", taskId);
 
     var getId = event.dataTransfer.getData("text/plain");
-    console.log("getId:", getId, typeof getId);
 };
 
+//when an object is dragged over, determines if it is a list. if so allows dropping of the task
 var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
@@ -184,7 +188,29 @@ var dropZoneDragHandler = function(event) {
 };
 
 
-//Evenet handlers
+//when a task is 
+var dropTaskHandler = function(event) {
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+
+    if (statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0;
+    }
+    else if (statusType === "tasks-in-progress") {
+        statusSelectEl.selectedIndex = 1;
+    }
+    else if (statusType === "tasks-completed") {
+        statusSelectEl.selectedIndex = 2;
+    }
+
+    dropZoneEl.appendChild(draggableElement);
+};
+
+
+//Event listener
 ////calls the taskFormHandler in the event of a form submission
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -199,3 +225,6 @@ pageContentEL.addEventListener("dragstart", dragTaskHandler);
 
 ////calls the dropZoneDragHandler when you drag a draggable element over the zone.
 pageContentEL.addEventListener("dragover", dropZoneDragHandler);
+
+////calls the dropTaskHandler when you drop a draggable element into the zone.
+pageContentEL.addEventListener("drop", dropTaskHandler);
